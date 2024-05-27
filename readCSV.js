2bcbb641973f427858269798804cd150f7ac1659
Martin <22581506+dis-lekker-by-die-see@users.noreply.github.com>
2024-05-27 15:32:39 +0900

    Fix container error

--- readCSV.js
+++ readCSV.js
@@ -1,39 +1,5 @@
 
 let events = {};
-
-
-//function loadEventCSV() {
-
-    // document.addEventListener('DOMContentLoaded', function() {
-    //     fetch('/仮日程.csv')
-    //         .then(response => response.text())
-    //         .then(text => {
-
-    //             const lines = text.trim().split('\n');
-    //             lines.forEach(line => {
-    //                 const [scheduleNumber, scheduleDate, eventNumber, category, eventCode,
-    //                     eventName, eventDescription, priceCode, price] = line.split(',');
-    //             if (scheduleDate && scheduleNumber && eventName) {
-    //                 events[scheduleNumber.trim()] = {
-    //                     scheduleDate: scheduleDate.trim(),
-    //                     eventName: eventName.trim(),
-    //                     category: category.trim(),
-    //                     eventCode: eventCode.trim(),
-    //                     eventDescription: eventDescription.trim(),
-    //                     priceCode: priceCode.trim(),
-    //                     price: parseFloat(price.trim())
-    //                 };
-    //             }
-    //         });
-    //         console.log('Events loaded:', events); // For debugging purposes
-    //         })
-    //         .catch(error => console.error('Error loading the CSV file:', error));
-    // });
-
-
-////////////////////
-
-
 document.addEventListener('DOMContentLoaded', function() {
     fetch('/仮日程.csv')
         .then(response => response.text())
@@ -93,20 +59,3 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .catch(error => console.error('Error loading the CSV file:', error));
 });
-
-
-
-
-///////////
-
-// function displayData(events) {
-//     const output = document.getElementById('output');
-//     output.innerHTML = '<table>' + events.map(row => 
-//         `<tr>${row.map(cell => `<td>${cell}</td>`).join('')}</tr>`
-//     ).join('') + '</table>';
-// }
-
-
-
-//loadEventCSV();
-//displayData(events);
